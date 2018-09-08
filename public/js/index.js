c6714c95c74dd853b0f46c333f05e106190235ca
memolroxo <masoudline@gmail.com>
2018-09-08 13:01:23 +0430

    Add mustache lib

--- public/js/index.js
+++ public/js/index.js
@@ -2,23 +2,20 @@ var socket = io();
 
 socket.on('connect', function(){
     console.log('Connected to server');
-
-    // socket.emit('createMessage', {
-    //     from: "masoud",
-    //     text: "Hey, it's work"
-    // })
 })
 
 socket.on('disconnect', function(){
     console.log('Disconnected from server');
 })
 socket.on('newMessage', function(message){
-    console.log('New Message', message)
-
-    var li = jQuery('<li></li>')
-    li.text(`${message.from}: ${message.text}`)
-
-    jQuery('#messages').append(li)
+    var formatedTime = moment(message.createdAt).format('hh:mm a')
+    var template = jQuery('#message-template').html()  
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formatedTime
+    })
+    jQuery('#messages').append(html)
 })
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
@@ -50,11 +47,12 @@ loactionButton.on('click', function(){
 })
 
 socket.on('newLocationMessage', function(message){
-    var li = jQuery('<li></li>')
-    var a = jQuery('<a target="_blank">My Current Location</a>')
-
-    li.text(`${message.from}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li)
+    var formatedTime = moment(message.createdAt).format('hh:mm a')
+    var template = jQuery('#location-message-template').html()  
+    var html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formatedTime
+    })
+    jQuery('#messages').append(html)
 })
